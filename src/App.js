d67fc489dd003b4b968ffec3428001bb46cd6887
blueyuyu <ublueyuyuyu@gmail.com>
2025-03-29 13:42:17 +0800

    🎉 init(all): 初始化项目

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="calculator-gird">
+      <div className="out-put">
+        <div className="output-text">1000</div>
+        <div className="output-result">200</div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
