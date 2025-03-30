c8806c26d9278179c358785f3aff7f28168f4013
blueyuyu <ublueyuyuyu@gmail.com>
2025-03-30 13:36:04 +0800

    ✨ feat(app): 样式的初步调整

--- src/App.js
+++ src/App.js
@@ -2,10 +2,34 @@ import './App.css';
 
 function App() {
   return (
-    <div className="calculator-gird">
-      <div className="out-put">
-        <div className="output-text">1000</div>
-        <div className="output-result">200</div>
+    <div className='container'>
+      <div className="calculator-grid">
+        <div className="out-put">
+          <div className="output-text">1000</div>
+          <div className="output-result">200</div>
+        </div>
+        <button className='btn ac'>AC</button>
+        <button className='btn'>DEL</button>
+        <button className='btn'>÷</button>
+
+        <button className='btn'>1</button>
+        <button className='btn'>2</button>
+        <button className='btn'>3</button>
+        <button className='btn'>*</button>
+
+        <button className='btn'>4</button>
+        <button className='btn'>5</button>
+        <button className='btn'>6</button>
+        <button className='btn'>+</button>
+
+        <button className='btn'>7</button>
+        <button className='btn'>8</button>
+        <button className='btn'>9</button>
+        <button className='btn'>-</button>
+
+        <button className='btn'>.</button>
+        <button className='btn'>0</button>
+        <button className='btn equal'>=</button>
       </div>
     </div>
   );
